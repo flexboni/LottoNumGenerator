b862708faaf0c126d0a32525dcdd9bbf3005d289
Boni <kgb8594@gmail.com>
2020-02-14 08:46:31 +0900

    이름의 HashCode 를 기준으로 랜덤 6개 수 구하는 로직 적용

--- app/src/main/java/com/boni/lottonumgenerator/NameActivity.kt
+++ app/src/main/java/com/boni/lottonumgenerator/NameActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_name.*
+import kotlin.random.Random
 
 class NameActivity : AppCompatActivity() {
 
@@ -12,11 +13,38 @@ class NameActivity : AppCompatActivity() {
         setContentView(R.layout.activity_name)
 
         btnActNameGenerateNum.setOnClickListener {
-            startActivity(Intent(this@NameActivity, ResultActivity::class.java))
+            val intent = Intent(this@NameActivity, ResultActivity::class.java)
+
+            // intent 의 결과 데이터를 전달한다.
+            // int 의 리스트를 전달하므로 putIntegerArrayListExtra 를 사용한다.
+            // 전달하는 리스트는 이름의 해시코드로 생성한 로또 번호
+            intent.putIntegerArrayListExtra("result", ArrayList(getLottoNumbersFromHash(etActName.text.toString())))
+
+            startActivity(intent)
         }
 
         btnActNameBack.setOnClickListener {
             finish()
         }
+    }
+
+    /**
+     * 입력받은 이름에 대한 해시코드를 사용하여 로또 번호를 섞고 결과를 반환한다.
+     */
+    fun getLottoNumbersFromHash(name: String): MutableList<Int> {
+        // 1~45번에 로또 번호를 저장할 리스트 생성
+        val list = mutableListOf<Int>()
+
+        // 1~45까지 for 문을 돌면서 리스트에 로또 번호 저장
+        for (number in 1..45){
+            list.add(number)
+        }
+
+        // 리스트를 무작위로 섞는다. 이때 섞는 기준으로 Random(SEED) 를 사용한다.
+        // SEED 값은 전달받은 이름의 해시코드를 사용한다.
+        list.shuffle(Random(name.hashCode().toLong()))
+
+        // 리스트를 앞에서 부터 순서대로 6개를 짤라 결과 반환
+        return list.subList(0, 6)
     }
 }
