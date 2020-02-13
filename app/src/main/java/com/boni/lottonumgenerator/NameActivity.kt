211efe13e72f861f43705819a7f424798e242ff0
Boni <kgb8594@gmail.com>
2020-02-14 08:50:54 +0900

    이름과 날짜가 조합된 HashCode 를 기준으로 랜덤 6개 수 구하는 로직 적용

--- app/src/main/java/com/boni/lottonumgenerator/NameActivity.kt
+++ app/src/main/java/com/boni/lottonumgenerator/NameActivity.kt
@@ -4,6 +4,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_name.*
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
 import kotlin.random.Random
 
 class NameActivity : AppCompatActivity() {
@@ -40,9 +43,17 @@ class NameActivity : AppCompatActivity() {
             list.add(number)
         }
 
+        // SimapleDateFormat 은 날짜의 시간값을 포맷화된 텍스트 형태로 바꿔주는 클래스
+        // 현재 Date 의 'yyyy-MM-dd' 문자열과 이름 문자열을 합친다
+        val targetString = SimpleDateFormat("yyyy-MM-dd", Locale.KOREA).format(Date()) + name
+
+//        // 리스트를 무작위로 섞는다. 이때 섞는 기준으로 Random(SEED) 를 사용한다.
+//        // SEED 값은 전달받은 이름의 해시코드를 사용한다.
+//        list.shuffle(Random(name.hashCode().toLong()))
+
         // 리스트를 무작위로 섞는다. 이때 섞는 기준으로 Random(SEED) 를 사용한다.
         // SEED 값은 전달받은 이름의 해시코드를 사용한다.
-        list.shuffle(Random(name.hashCode().toLong()))
+        list.shuffle(Random(targetString.hashCode().toLong()))
 
         // 리스트를 앞에서 부터 순서대로 6개를 짤라 결과 반환
         return list.subList(0, 6)
