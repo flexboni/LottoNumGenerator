a32e6d0ca2aaf076470422bf48080bfdfc5d9903
Boni <kgb8594@gmail.com>
2020-02-13 09:15:19 +0900

    Random 으로 1~46 번호 생성하는 로직 구현
    ResultActivity 로 전달할 랜덤 수 추출 로직 구현.

--- app/src/main/java/com/boni/lottonumgenerator/ConstellationActivity.kt
+++ app/src/main/java/com/boni/lottonumgenerator/ConstellationActivity.kt
@@ -1,12 +1,18 @@
 package com.boni.lottonumgenerator
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_constellation.*
 
 class ConstellationActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_constellation)
+
+        btnGenerator.setOnClickListener {
+            startActivity(Intent(this@ConstellationActivity, ResultActivity::class.java))
+        }
     }
 }
--- app/src/main/java/com/boni/lottonumgenerator/NameActivity.kt
+++ app/src/main/java/com/boni/lottonumgenerator/NameActivity.kt
@@ -1,12 +1,22 @@
 package com.boni.lottonumgenerator
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_name.*
 
 class NameActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_name)
+
+        btnActNameGenerateNum.setOnClickListener {
+            startActivity(Intent(this@NameActivity, ResultActivity::class.java))
+        }
+
+        btnActNameBack.setOnClickListener {
+            finish()
+        }
     }
 }
